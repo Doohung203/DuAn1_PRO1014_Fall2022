eb91705dab4e47ab96e77f3e24bb6e6e17f78962
Trần Hữu Hưng <doohung2003@gmail.com>
2022-11-15 10:41:05 +0700

    hungcommit

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="admin mx-auto bg-green-500 items-center flex ">
+        <img src="./images/zyro-mouth.png" alt="" width="10%">
+        <h2 class="p-4 font-bold text-2xl font-">Health Clinic</h2>
+
+    </div>
+
+</body>
+</html>
