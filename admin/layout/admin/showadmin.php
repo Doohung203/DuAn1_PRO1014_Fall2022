ca60e537f97a53859ee5c90a26c289c3aa7730b5
Tran Huu Hung <88076873+Doohung203@users.noreply.github.com>
2022-11-26 09:51:16 +0700

    hungcommit

--- admin/layout/admin/showadmin.php
+++ admin/layout/admin/showadmin.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../../dao/connect.php";
+require_once "../../../dao/connect.php";
 
 //show
 $sql = "SELECT * FROM admin";
@@ -54,13 +54,13 @@ $admin = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <!-- Main -->
         <section class="manage grid grid-cols-5 gap-4 my-4 ">
             <div class="menu  leading-10 bg-[#30536D]">
-                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="index.html"><i class="fa-solid fa-user p-4"> </i> Quản lý tài khoản nhân sự</a>
-                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="quanLyBenhNhan.html"><i class="fa-solid fa-hospital-user p-4"></i> Quản lý bệnh nhân</a>
-                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="quanLyHoSo.html"><i class="fa-solid fa-file-pen p-4"></i> Quản lý hồ sơ</a>
-                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="quanLyBinhLuan.html"><i class="fa-solid fa-comments p-4"></i> Quản lý bình luận</a>
-                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="quanLyBooking.html"><i class="fa-solid fa-calendar-days p-4"></i> Quản lý booking</a>
-                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="quanLyLichKham.html"><i class="fa-sharp fa-solid fa-clock p-4"></i> Quản lý lịch khám</a>
-                <a class="block py-[35px] font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-red-500" href="quanLyKetLuan.html"><i class="fa-solid fa-comment-medical p-4"></i>Kết luận của bác sĩ</a>
+                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="showadmin.php"><i class="fa-solid fa-user p-4"> </i> Quản lý tài khoản nhân sự</a>
+                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="../../danh-muc/quanLyBenhNhan.php"><i class="fa-solid fa-hospital-user p-4"></i> Quản lý bệnh nhân</a>
+                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="../../danh-muc/quanLyHoSo.php"><i class="fa-solid fa-file-pen p-4"></i> Quản lý hồ sơ</a>
+                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="../../danh-muc/quanLyBinhLuan.php"><i class="fa-solid fa-comments p-4"></i> Quản lý bình luận</a>
+                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="../../danh-muc/quanLyBooking.php"><i class="fa-solid fa-calendar-days p-4"></i> Quản lý booking</a>
+                <a class="block py-[35px] px-4 font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-yellow-500" href="../../danh-muc/quanLyLichKham.php"><i class="fa-sharp fa-solid fa-clock p-4"></i> Quản lý lịch khám</a>
+                <a class="block py-[35px] font-bold text-lg text-white to-yellow-300 hover:bg-white hover:text-green-300 hover:border hover:border-red-500" href="../../danh-muc/quanLyKetLuan.php"><i class="fa-solid fa-comment-medical p-4"></i>Kết luận của bác sĩ</a>
             </div>
             <section class="col-span-4 border-2-sm bg-gray-200 shadow-inherit">
                 <h2 class="py-6 font-bold text-3xl text-center text-gray-600 uppercase">Quản lý tài khoản nhân sự</h2>
