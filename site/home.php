7457a92d58bdb84bfebd95d6349c1c0c6648b720
Tran Huu Hung <88076873+Doohung203@users.noreply.github.com>
2022-12-04 10:41:28 +0700

    hung

--- site/home.php
+++ site/home.php
@@ -31,6 +31,7 @@
                     <div class="home-doctument text-white font-bold">
                         <ul>
                             <li>
+                                <!-- login-logout -->
                                 <div >
                                     <div class="dropdown inline-block relative">
                                         <button class="bg-green-400 text-white font-semibold py-2 px-8 rounded inline-flex items-center">
@@ -45,9 +46,8 @@
                                             <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-3 px-4 block whitespace-no-wrap" href="logout.php">Đăng xuất</a></li>
                                         </ul>
                                     </div>
-
                                 </div>
-                                <!--  -->
+                                <!--  End-login-logout -->
                             </li>
                         </ul>
                     </div>
@@ -59,11 +59,8 @@
             <div class="phone flex my-4 uppercase">
                 <p> <i class="fa-sharp fa-solid fa-phone-volume"></i> Khoa cấp cứu : <a href="" class="text-red-500 font-bold">098775423</a></p>
             </div>
-            <div class="search relative block">
-                <i class="fas fa-search absolute inset-y-0 left-0 flex items-center pl-2 "></i>
-                <input type="search" placeholder="Tìm kiếm" class="w-[70%] placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-[3px] pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"> 
-            </div>
         </div>
+<!-- End-PhoneNumber - Search -->
 <!-- Menu-top -->
         <div class="menu bg-green-500 rounded-md">
             <div class="menu-nav max-w-5xl mx-auto relative ">
