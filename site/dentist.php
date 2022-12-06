95bb8602e611142a56432d16199744fa479178f3
Lý Tự Lẫm <118238134+litproducer@users.noreply.github.com>
2022-12-06 09:52:48 +0700

    lam

--- site/dentist.php
+++ site/dentist.php
@@ -24,7 +24,6 @@ $doctor = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
-<<<<<<< HEAD
     <section class="container mx-auto">
         <!--Main-header -->
         <div class="contianer mx-auto">
@@ -74,93 +73,6 @@ $doctor = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <div class="container max-w-7xl mx-auto my-4">
                 <h2 class="font-bold text-2xl border-b-2 uppercase">Đội ngũ chuyên gia</h2>
                 <div class="grid grid-cols-4 gap-8 my-4 text-center">
-=======
-<section class="container mx-auto">
-    <!--Main-header -->
-    <div class="mx-auto">
-        <div class="Header">
-            <!-- Lcation - SignIn - SignUp -->
-            <div class="flex justify-between items-center px-4 py-6 border-b-2 h-4">
-                <div class="location">
-                    <p> <i class="fa-regular fa-map"></i> Nơi ở hiện tại : Trịnh Văn Bô, Nam Từ Liêm, Hà Nội</p>
-                </div>
-                <div class="home-document text-white font-bold">
-                    <ul>
-                        <!-- login-logout -->
-                        <li>
-                            <div >
-                                    <div class="dropdown inline-block relative">
-                                        <button class="bg-green-400 text-white font-semibold py-2 px-8 rounded inline-flex items-center">
-                                            <span class="mr-1"> <i class="fa-regular fa-user"></i></span>
-                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                                            </svg>
-                                        </button>
-                                        <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-                                            <li class=""><a class="rounded-t bg-gray-200 hover:bg-gray-400 py-3 px-4 block whitespace-no-wrap" href="login.php">Đăng nhập</a></li>
-                                            <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-3 px-4 block whitespace-no-wrap" href="register.php">Đăng ký</a></li>
-                                            <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-3 px-4 block whitespace-no-wrap" href="logout.php">Đăng xuất</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <!--  End-login-logout -->
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!-- PhoneNumber - Search -->
-        <div class="banner-top flex justify-between items-center px-4">
-            <div class="phone flex my-4 uppercase">
-                <p> <i class="fa-sharp fa-solid fa-phone-volume"></i> Khoa cấp cứu : <a href="" class="text-red-500 font-bold">098775423</a></p>
-            </div>
-        </div>
-        <!-- Menu-top -->
-        <div class="menu bg-green-500 rounded-md">
-            <div class="menu-nav max-w-5xl mx-auto relative ">
-                <ul>
-                    <li class="flex justify-between items-center font-bold text-white">
-                        <a class="py-4 px-2 hover:bg-sky-100 hover:text-green-500  hover:rounded-md" href="home.php">Trang chủ</a>
-                        <a class="py-4 px-2 hover:bg-sky-100 hover:text-green-500 hover:rounded-md" href="priceListService.php">Bảng giá dịch vụ</a>
-                        <a class="py-4 px-2 hover:bg-sky-100 hover:text-green-500 hover:rounded-md" href="">Đăng ký dịch vụ</a>
-                        <a style="font-family: Inter-font;" class="py-4 px-2 text-4xl uppercase hover:bg-sky-100 hover:text-green-500" href="home.php">Health Clinic</a>
-                        <a class="py-4 px-2 hover:bg-sky-100 hover:text-green-500 hover:rounded-md" href="dentist.php">Đội ngũ chuyên gia</a>
-                        <a class="py-4 px-2 hover:bg-sky-100 hover:text-green-500 hover:rounded-md" href="contact.php">Liên hệ</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- End-Header -->
-
-    <!--Main-content  -->
-        <div class="container max-w-7xl mx-auto my-4">
-            <h2 class="font-bold text-2xl border-b-2 uppercase">Đội ngũ chuyên gia</h2>
-            <div class="grid grid-cols-4 gap-8 my-4 text-center">
-                <div class="dentist">
-                    <a href="detailDentist.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs1.jpg" alt=""></a>
-                    <a class="text-green-500 font-bold text-[14px] " href="detailDentist.php">PGS.TS.TRẦN HỮU HƯNG</a>
-                    <p class="">Giám đốc Bệnh viện</p>
-                </div>
-                <div class="dentist">
-                    <a href="detailDentist.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs2.jpg" alt=""></a>
-                    <a class="text-green-500 font-bold text-[14px] " href="detailDentist.php">TS. MAI VĂN NAM</a>
-                    <p class="">Phó Giám đốc Bệnh viện</p>
-                </div>
-                <div class="dentist">
-                    <a href="detailDentist.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs3.jpg" alt=""></a>
-                    <a class="text-green-500 font-bold text-[14px] " href="detailDentist.php">TS. LÝ TỰ LẪM</a>
-                    <p class="">Phó Giám đốc Bệnh viện</p>
-                </div>
-                <div class="dentist">
-                    <a href="detailDentist.php"><img src="https://ranghammat.org.vn/Upload/Avartar/ts-nguyen-thi-van-anh.jpg" alt=""></a>
-                    <a class="text-green-500 font-bold text-[14px] " href="detailDentist.php">TS. NGUYỄN THỊ VÂN ANH</a>
-                    <p class="">Trưởng khoa Răng Trẻ em</p>
-                </div>
-            </div>
-        </div>
-    <!-- End-Main-content  -->
->>>>>>> ddcc907c76c59ec86727d06187d2b2e252e33ae2
 
                     <?php foreach ($doctor as $bs) : ?>
                         <a href="detailDentist.php?id=<?= $bs['id']?>" class="dentist">
