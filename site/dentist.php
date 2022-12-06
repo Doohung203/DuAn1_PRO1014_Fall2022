eb9c72b779ce8d595a41ad0dde546a3f4d8e0901
Tran Huu Hung <88076873+Doohung203@users.noreply.github.com>
2022-12-06 10:07:47 +0700

    hung

--- site/dentist.php
+++ site/dentist.php
@@ -7,7 +7,14 @@
     <title>Dentist</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/8ddf476500.js" crossorigin="anonymous"></script>
-
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../font-css/font-logo.css">
+        <style>
+            .dropdown:hover .dropdown-menu {
+                display: block;
+            }
+        </style>
 </head>
 <body>
 <section class="container mx-auto">
@@ -78,12 +85,12 @@
                     <p class="">Giám đốc Bệnh viện</p>
                 </div>
                 <div class="dentist">
-                    <a href="detailDentist.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs2.jpg" alt=""></a>
+                    <a href="detailDentist1.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs2.jpg" alt=""></a>
                     <a class="text-green-500 font-bold text-[14px] " href="detailDentist.php">TS. MAI VĂN NAM</a>
                     <p class="">Phó Giám đốc Bệnh viện</p>
                 </div>
                 <div class="dentist">
-                    <a href="detailDentist.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs3.jpg" alt=""></a>
+                    <a href="detailDentist2.php"><img src="https://ranghammat.org.vn/Upload/Avartar/anh-bs3.jpg" alt=""></a>
                     <a class="text-green-500 font-bold text-[14px] " href="detailDentist.php">TS. LÝ TỰ LẪM</a>
                     <p class="">Phó Giám đốc Bệnh viện</p>
                 </div>
--- site/detailDentist.php
+++ site/detailDentist.php
@@ -16,6 +16,14 @@ $doctor = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../font-css/font-logo.css">
+        <style>
+            .dropdown:hover .dropdown-menu {
+                display: block;
+            }
+        </style>
 </head>
 
 <body>
