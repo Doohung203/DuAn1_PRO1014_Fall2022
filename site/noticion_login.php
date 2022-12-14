e86bcf7ff1c401614940360b51fe38d8fa9f3a0a
daivmph25545 <115127976+daivmph25545@users.noreply.github.com>
2022-12-14 17:33:31 +0700

    .

--- site/noticion_login.php
+++ site/noticion_login.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/8ddf476500.js" crossorigin="anonymous"></script>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../font-css/font-logo.css">
 </head>
 <body>
     <!--Main-header -->
